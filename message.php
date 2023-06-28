e14eddc30466409d9d342bb62e974f49f7972273
Andrick Takisawa <andricktakisawa@MacBook-Pro-de-Andrick.local>
2023-06-28 10:06:20 -0600

    Commit

--- message.php
+++ message.php
@@ -1,37 +1,67 @@
-<?php
-// Leer los mensajes del archivo JSON
-$messages = json_decode(file_get_contents('messages.json'));
-
-// Comprobar si hay una cookie con el último mensaje visto
-if (isset($_COOKIE['lastMessage'])) {
-    $lastMessage = $_COOKIE['lastMessage'];
-} else {
-    $lastMessage = null;
-}
-
-// Seleccionar un mensaje al azar que no sea igual al último
-do {
-    $message = $messages[array_rand($messages)];
-} while ($message === $lastMessage);
-
-// Establecer una cookie con el mensaje de hoy para el próximo día
-setcookie('lastMessage', $message, time() + (86400 * 30), "/"); // 86400 = 1 día
-
-?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Mensajito Diario</title>
     <link rel="stylesheet" href="style.css"> <!-- Reemplaza con el nombre de tu archivo CSS -->
+    <script>
+        window.onload = function() {
+            // Cargar los mensajes
+            fetch('messages.json')
+                .then(response => response.json())
+                .then(messages => {
+                    let now = new Date();
+
+                    // Conversiones a la hora de CDMX (UTC -5)
+                    let offset = now.getTimezoneOffset() / 60;
+                    now.setHours(now.getHours() + (offset + 5));
+
+                    let lastUpdate = localStorage.getItem('nextUpdate');
+                    let messageIndex = localStorage.getItem('lastMessageIndex');
+
+                    // Si no se ha alcanzado la hora de actualización, mostrar el mensaje anterior
+                    if (lastUpdate && now.getTime() < lastUpdate) {
+                        document.getElementById('message').innerText = messages[messageIndex];
+                        return;
+                    }
+
+                    // Seleccionar un índice de mensaje al azar que no sea igual al último
+                    do {
+                        messageIndex = Math.floor(Math.random() * messages.length);
+                    } while (messageIndex == localStorage.getItem('lastMessageIndex'));
+
+                    // Guardar el índice del mensaje de hoy para la próxima vez
+                    localStorage.setItem('lastMessageIndex', messageIndex);
+
+                    // Mostrar el mensaje en la página
+                    document.getElementById('message').innerText = messages[messageIndex];
+
+                    // Calcular la próxima fecha de actualización
+                    let nextUpdate = new Date(now);
+
+                    if (now.getHours() < 5) {
+                        nextUpdate.setHours(10, 0, 4, 0);
+                    } else if (now.getHours() < 19) {
+                        nextUpdate.setHours(19, 0, 0, 0);
+                    } else {
+                        nextUpdate.setDate(nextUpdate.getDate() + 1);
+                        nextUpdate.setHours(5, 0, 0, 0);
+                    }
+
+                    // Guardar la próxima fecha de actualización
+                    localStorage.setItem('nextUpdate', nextUpdate.getTime());
+                })
+                .catch(error => console.error('Error:', error));
+        }
+    </script>
 </head>
 
 <body>
     <div class="message">
         <h2>Mensajito Diario</h2>
-        <p><?php echo htmlspecialchars($message); ?></p>
-        <a href="index.php" class="button">Regresar</a>
+        <p id="message"></p>
+        <a href=" index.php" class="button">Regresar</a>
+        <p style="font-size: 13px; color: #848884;"><span style="color: #0d0d20;">*</span>Nuevo mensaje todos los días a las 5:00 y 19:000 hrs.</p>
     </div>
 </body>
 
